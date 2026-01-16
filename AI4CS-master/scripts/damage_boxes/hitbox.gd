2ce907619864163b9d4303d2f52ce7b8d15ec079
JuicyTomato <80514705+JuicyTomato@users.noreply.github.com>
2026-01-16 14:53:15 +0100

    Fixed scene Golem and Wraith

---
+++ AI4CS-master/scripts/damage_boxes/hitbox.gd
@@ -0,0 +1,8 @@
+class_name Hitbox extends Area2D
+
+@export var damage: = 1
+@export var knockback_amount: = 200
+@export var knockback_direction: Vector2
+
+# Per il salvataggio
+@export var enemy_id: int
