67c1ed0ea2b9e61a76bd89949439a385e0c07f07
JuicyTomato <80514705+JuicyTomato@users.noreply.github.com>
2026-01-18 13:29:57 +0100

    Delete AI4CS-master directory

--- AI4CS-master/scripts/gui/settings_scene.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-#il menu viene aperto da 2 bottoni diversi, utilizzando i signal puoi tornare alla scena precedente senza usare get_tree.change_scene_to_file()
-signal closed
-
-func _on_return_button_pressed() -> void:
-	emit_signal("closed")
-	queue_free()
