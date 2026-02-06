8366c9c509ca3c4802fdbd8a4a8d3c84ae5d1b3f
JuicyTomato <80514705+JuicyTomato@users.noreply.github.com>
2026-02-06 15:40:53 +0100

    Delete AI4CS-master directory

--- AI4CS-master/scripts/damage_boxes/hitbox.gd
+++
@@ -1,8 +0,0 @@
-class_name Hitbox extends Area2D
-
-@export var damage: = 1
-@export var knockback_amount: = 200
-@export var knockback_direction: Vector2
-
-# Per il salvataggio
-@export var enemy_id: int
